ad003c39ab44dee52caefd0fb9b9e30d0247f571
Karlux007 <homeopatia369@gmail.com>
2025-06-27 18:54:59 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,1 @@
 
-import React from 'react'
-
-function App() {
-  return (
-    <div className="p-4 text-center">
-      <h1 className="text-3xl font-bold text-blue-600">H369 App Online</h1>
-      <p className="mt-2">Bem-vindo à aplicação de diagnóstico homeopático.</p>
-    </div>
-  )
-}
-
-export default App
