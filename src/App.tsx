1c418c9688d520d6f896f1fd6674fecb2d9f8dc6
Karlux007 <homeopatia369@gmail.com>
2025-06-27 17:44:19 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bem-vindo à App H369</h1>
+      <p>Consulta os teus Reinos Homeopáticos com base na tua data de nascimento.</p>
+    </div>
+  );
+}
