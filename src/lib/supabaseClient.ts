f22ff932643ed1c79e14621751a71b35b9000b7f
Karlux007 <homeopatia369@gmail.com>
2025-06-28 12:52:00 +0100

    Create supabaseClient.ts

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://[O_TEU_PROJECT_URL].supabase.co';
+const supabaseKey = '[A_TUA_PUBLIC_ANON_KEY]';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
