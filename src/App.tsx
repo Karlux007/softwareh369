90e86cffbd73b439f3bd1f51d7e07505d67fb951
Karlux007 <homeopatia369@gmail.com>
2025-07-12 19:04:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import Formulario from "./Formulario";
+
 export default function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ fontSize: 24, color: 'green' }}>✅ App carregada com sucesso!</h1>
-    </div>
-  );
+  return <Formulario />;
 }
