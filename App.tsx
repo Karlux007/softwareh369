5c5cc07ab945f6380c886886f921d7881c843a32
Karlux007 <homeopatia369@gmail.com>
2025-07-12 20:23:03 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
 import Formulario from "./Formulario";
 
 export default function App() {
-  return (
-    <main className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold text-center mb-6">H 3.6.9 - Cálculo de Reinos</h1>
-      <Formulario />
-    </main>
-  );
+  return <Formulario />;
 }
