70e24463bd2f23ee6569e21cc332b51bca16e3ce
Karlux007 <homeopatia369@gmail.com>
2025-07-12 14:14:54 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>✅ App carregada com sucesso!</h1>
+      <h1 style={{ fontSize: 24, color: 'green' }}>✅ App carregada com sucesso!</h1>
     </div>
   );
 }
